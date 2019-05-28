5b75efe0b71950df203a87cf4488d0fbe768827c
Alex Johnson <alexmj212@gmail.com>
2019-05-27 21:23:02 -0400

    dynamically sized clocks (sloppy)

--- scripts/Clock.js
+++ scripts/Clock.js
@@ -5,7 +5,7 @@ class Clock {
     }
 
     clock = {
-        radius: 250
+        radius: 0
     }
 
     secondHand = {
@@ -28,6 +28,19 @@ class Clock {
         y: 0
     }
 
+    getRadius() {
+        var radius = this.element.querySelector('.clock').getBoundingClientRect().width / 2;
+        this.setClockCenter(radius);
+        return radius;
+    }
+
+    setClockCenter(radius) {
+        var offset = this.element.querySelector('.second-hand').clientWidth / 2 + 1;
+        this.clock['center'] = { x: (radius - offset), y: (radius - offset) };
+        this.setTicks(radius);
+        return radius;
+    }
+
     setSeconds() {
 
         // grab second hand element
@@ -41,10 +54,10 @@ class Clock {
         this.secondHand.position += this.secondHand.dposition;
 
         // set x
-        secondHandElement.style.top = this.secondHand.x = this.clock.center.x + (this.clock.radius * Math.sin(this.secondHand.position)) + 'px';
+        secondHandElement.style.top = this.secondHand.x = this.clock.center.x + (this.getRadius() * Math.sin(this.secondHand.position)) + 'px';
 
         // set y
-        secondHandElement.style.left = this.secondHand.y = this.clock.center.y + (this.clock.radius * Math.cos(this.secondHand.position)) + 'px';
+        secondHandElement.style.left = this.secondHand.y = this.clock.center.y + (this.getRadius() * Math.cos(this.secondHand.position)) + 'px';
 
         if (date.getSeconds() === 0) {
             this.setMinutes();
@@ -82,29 +95,27 @@ class Clock {
 
     }
 
-    setTicks() {
+    setTicks(radius) {
 
         // grab hour hand element
         var tickElement = this.element.querySelector('.tick');
 
         // set x
-        tickElement.style.top = this.clock.center.x + (this.clock.radius * Math.sin(((-1) * Math.PI) / 2)) + tickElement.getBoundingClientRect().width + 2 + 'px';
+        tickElement.style.top = this.clock.center.x + (radius * Math.sin(((-1) * Math.PI) / 2)) + tickElement.getBoundingClientRect().width + 2 + 'px';
 
         // set y
-        tickElement.style.left = this.clock.center.y + (this.clock.radius * Math.cos(Math.PI / 2)) + tickElement.getBoundingClientRect().width + 1 + 'px';
+        tickElement.style.left = this.clock.center.y + (radius * Math.cos(Math.PI / 2)) + tickElement.getBoundingClientRect().width + 1 + 'px';
 
     }
 
     initialize() {
         var date = new Date();
-        this.clock.radius = this.element.querySelector('.clock').clientWidth / 2;
-        var offset = this.element.querySelector('.second-hand').clientWidth / 2 + 1;
-        this.clock['center'] = { x: (this.clock.radius - offset), y: (this.clock.radius - offset) };
 
         this.secondHand.position = (this.secondHand.dposition * (date.getSeconds() - 1)) - 90 * Math.PI / 180;
         this.minuteHand.position = (this.minuteHand.dposition * (date.getMinutes() - 1)) - 90 * Math.PI / 180;
         this.hourHand.position = (this.hourHand.dposition * (date.getHours() - 1)) - 90 * Math.PI / 180;
 
+        this.setClockCenter();
         this.setSeconds();
         this.setMinutes();
         this.setHours();
